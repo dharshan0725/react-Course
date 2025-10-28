276ee892b22f25f5f6a7521fa838537c86ad28f1
Pulichala Priyadarshan <pulicharladharshan479@gmail.com>
2025-10-28 15:24:11 +0530

    first commit

---
+++ Ecommerce-Project/src/App.jsx
@@ -0,0 +1,15 @@
+import { HomePage } from "./pages/HomePage";
+import "./App.css";
+import { Route, Routes } from "react-router";
+import CheckoutPage from "./pages/CheckoutPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+    </Routes>
+  );
+}
+
+export default App;
